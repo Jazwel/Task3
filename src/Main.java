7ef927d76942399b20b27db20eb230b6b4e71a53
DS <mc_sd@bk.ru>
2023-04-06 21:12:53 -0300

    Final

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,15 @@
 public class Main {
     public static void main(String[] args) {
         int payment = 500;
+        int balance = 50;
         int bonus = (payment / 100) + payment;
+        int total = bonus + balance;
         boolean correct = payment > 1000;
         boolean not = payment < 1000;
         int plus = 1000 - payment;
         if (correct) {
 
-            System.out.println(bonus);
+            System.out.println(total);
         } else if (not) {
             System.out.println(" Для получения бонусаПополните счет еще на");
             System.out.println(plus);
