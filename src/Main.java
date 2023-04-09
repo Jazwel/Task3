81e7dcfb805645a73c62dbc55e54a5d79ad3d29f
DS <mc_sd@bk.ru>
2023-04-09 12:38:09 -0300

    new fersion

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,22 @@
 public class Main {
     public static void main(String[] args) {
-        int payment = 500;
-        int balance = 50;
+        int payment = 2000;
+        int balance = 100;
         int bonus = (payment / 100) + payment;
         int total = bonus + balance;
         boolean enoughPayment = payment > 1000;
         boolean notEnough = payment < 1000;
+        int countBonus = (payment / 100);
         int plus = 1000 - payment;
         if (enoughPayment) {
-
+            System.out.println("Начисленный бонус:");
+            System.out.println(countBonus);
+            System.out.println("Сумма на счету:");
             System.out.println(total);
         } else {
-            System.out.println(" Для получения бонусаПополните счет еще на");
+            System.out.println("Оплата");
+            System.out.println(payment);
+            System.out.println(" Для получения бонуса Пополните счет еще на:");
             System.out.println(plus);
 
 
